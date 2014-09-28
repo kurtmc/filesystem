6240be9b9606a7a996908b225b98cc200af33a6e
Kurt McAlpine <kurt@linux.com>
2014-09-28 16:00:51 +1300

    Fixed up testcases.sh:

--- testcases.sh
+++ testcases.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 make
-./A2com < tests/test1 > testoutput/test1.out
-./A2com < tests/test2 > testoutput/test2.out
-./A2com < tests/test3 > testoutput/test3.out
-./A2com < tests/test4 > testoutput/test4.out
-./A2com < tests/test5 > testoutput/test5.out
-./A2com < tests/test6 > testoutput/test6.out
-./A2com < tests/testspaces > testoutput/testspaces.out
 
-diff testoutput/test1.out testoutput/test1 
+testcases=( test1 test2 test3 test4 test5 test6 testspaces )
+
+for test in "${testcases[@]}"
+do
+	./A2com < tests/$test > testoutput/$test.out
+	
+	echo "Diff $test"
+	diff testoutput/$test.out testoutput/$test
+done
