20c1cbbf6bf2ba98b15178d3f23cfbb1f46dbb11
Kurt McAlpine <kurt@linux.com>
2014-09-29 15:44:20 +1300

    Create a script to create the testcase output

---
+++ createoutput.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make
+
+testcases=( test1 test2 test3 test4 test5 test6 testspaces )
+
+for test in "${testcases[@]}"
+do
+	./A2com < tests/$test | sed 's/\(.*\)[A-Z][a-z]\{2\}.*[0-9]\{2\}:[0-9]\{2\}/\1Sep 28 12:34/g' > testoutput/$test
+done
--- testcases.sh
+++ testcases.sh
@@ -5,7 +5,7 @@ testcases=( test1 test2 test3 test4 test5 test6 testspaces )
 
 for test in "${testcases[@]}"
 do
-	./A2com < tests/$test > testoutput/$test.out
+	./A2com < tests/$test | sed 's/\(.*\)[A-Z][a-z]\{2\}.*[0-9]\{2\}:[0-9]\{2\}/\1Sep 28 12:34/g' > testoutput/$test.out
 	
 	echo "Diff $test"
 	diff testoutput/$test.out testoutput/$test
